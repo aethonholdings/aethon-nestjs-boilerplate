42d5b875583e610afda5ba4435f9ee1f6f110a32
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-10-04 21:18:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# aethon-nestjs-boilerplate
+
+## Description
+
+Aethon Holdings Nestjs REST API boilerplate
+
+## Included
+
+1. NestJS core
+2. Prettier
+3. Jest
+4. nestjs/config and related scripts
---
+++ src/modules/root/root.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { ExampleModule } from "../example/example.module";
+import { ConfigModule } from "@nestjs/config";
+import environment from "../../../env/env";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            load: [environment],
+            isGlobal: true
+        }),
+        ExampleModule
+    ],
+    controllers: [],
+    providers: []
+})
+export class RootModule {}
