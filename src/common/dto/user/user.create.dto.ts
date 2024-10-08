44e080101ed75b4346c336c8f66da7e206e0d225
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-10-09 00:05:21 +0100

    Fix: User interface definition corrected

--- src/common/dto/user/user.create.dto.ts
+++ src/common/dto/user/user.create.dto.ts
@@ -1,6 +1,6 @@
 import { UserInterface } from "src/common/interfaces/user.interface";
 
-export class UserCreateDTO implements UserInterface{
+export class UserCreateDTO implements Partial<UserInterface>{
     firstName: string;
     lastName: string;
     isActive: boolean;
--- src/common/interfaces/user.interface.ts
+++ src/common/interfaces/user.interface.ts
@@ -1,5 +1,5 @@
 export interface UserInterface {
-    id?: number;
+    id: number;
     firstName: string;
     lastName: string;
     isActive: boolean;
