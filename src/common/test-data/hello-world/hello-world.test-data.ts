f721b557f8a76195f91e121afce769e3688da5fa
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-10-08 23:54:16 +0100

    Removed hello-world controller

--- src/common/test-data/hello-world/hello-world.test-data.ts
+++
@@ -1,6 +0,0 @@
-import { HelloWorldDTOGet } from "../../dto/hello-world/hello-world.get.dto";
-
-export const helloResponse: HelloWorldDTOGet = {
-    apiVersion: "0.1",
-    dev: true
-}
--- src/modules/example/example.module.ts
+++ src/modules/example/example.module.ts
@@ -1,8 +1,7 @@
 import { Module } from "@nestjs/common";
-import { HelloWorldModule } from "./endpoints/hello-world/hello-world.module";
 import { UserModule } from './endpoints/user/user.module';
 
 @Module({
-    imports: [HelloWorldModule, UserModule]
+    imports: [UserModule]
 })
 export class ExampleModule {}
