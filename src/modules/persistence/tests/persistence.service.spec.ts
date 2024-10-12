ac3f3967872d2571b8068cdedf8db236ec4c508a
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-10-12 20:42:42 +0100

    db and persistence service tests made entity agnostic

--- src/modules/persistence/tests/persistence.service.spec.ts
+++ src/modules/persistence/tests/persistence.service.spec.ts
@@ -4,84 +4,92 @@ import { DatabaseService } from "../services/database.service";
 import * as databaseConfig from "../mocks/datasource.config.mock";
 import { Example } from "src/common/classes/entities/example.entity";
 import { DataSource } from "typeorm";
-import { ExampleCreateDTO } from "src/common/classes/dto/example/example.create.dto";
 import { exampleTestData, paginateConfig } from "src/common/test-data/example.test-data";
+import { EntityClassOrSchema } from "@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type";
 
 describe("PersistenceService", () => {
     let service: PersistenceService;
     let dataSource: DataSource;
-    let testData: ExampleCreateDTO[];
+    let testData: any[];
+    let tests: { entity: EntityClassOrSchema; data: any[] }[] = [
+        {
+            entity: Example,
+            data: exampleTestData
+        }
+    ];
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            imports: databaseConfig.getImports([Example]),
-            providers: [PersistenceService, DatabaseService]
-        }).compile();
-
-        service = module.get<PersistenceService>(PersistenceService);
-        dataSource = module.get<DataSource>(DataSource);
-        testData = exampleTestData;
-    });
-
-    it("should be defined", () => {
-        expect(service).toBeDefined();
-    });
-
-    it("should create", async () => {
-        const example = JSON.parse(JSON.stringify(testData[0]));
-        await service.create(Example, example).then((result) => {
-            expect(result).toBeDefined();
-            expect(result.firstName).toEqual(example.firstName);
-            expect(result.lastName).toEqual(example.lastName);
-            expect(result.isActive).toEqual(example.isActive);
-            return result;
+    for (let test of tests) {
+        beforeEach(async () => {
+            const module: TestingModule = await Test.createTestingModule({
+                imports: databaseConfig.getImports([test.entity]),
+                providers: [PersistenceService, DatabaseService]
+            }).compile();
+            dataSource = module.get<DataSource>(DataSource);
+            service = module.get<PersistenceService>(PersistenceService);
+            testData = test.data;
         });
-    });
 
-    it("should find one", async () => {
-        const example = JSON.parse(JSON.stringify(testData[0]));
-        const created = await service.create(Example, example);
-        await service.findOne(Example, { where: { id: created.id } }).then((result) => {
-            expect(result).toBeDefined();
-            expect(result.firstName).toEqual(example.firstName);
-            expect(result.lastName).toEqual(example.lastName);
-            expect(result.isActive).toEqual(example.isActive);
-            return result;
+        it("should create", async () => {
+            const example = JSON.parse(JSON.stringify(testData[0]));
+            await service.create(test.entity, example).then((result) => {
+                expect(result).toBeDefined();
+                for (let key in result) {
+                    expect(result[key]).toEqual(example[key]);
+                }
+            });
         });
-    });
 
-    it("should find all", async () => {
-        await Promise.all(
-            testData.map(async (example) => {
-                return await service.create(Example, example);
-            })
-        );
-        await service.findAll(Example, { path: "test" }, paginateConfig).then((result) => {
-            expect(result).toBeDefined();
-            expect(result.data).toBeDefined();
-            expect(result.data.length).toEqual(testData.length);
-            return result;
+        it("should find one", async () => {
+            const example = JSON.parse(JSON.stringify(testData[0]));
+            const created = await service.create(test.entity, example);
+            await service.findOne(test.entity, { where: { id: created.id } }).then((result) => {
+                expect(result).toBeDefined();
+                for (let key in result) {
+                    expect(result[key]).toEqual(example[key]);
+                }
+            });
         });
-    });
 
-    it("should update", async () => {
-        const example = JSON.parse(JSON.stringify(testData[0]));
-        const updatedExample = { ...example, firstName: "Updated" };
-        const created = await service.create(Example, example);
-        await service.update(Example, created.id, updatedExample).then((result) => {
-            expect(result).toBeNull();
-            return result;
+        it("should find all", async () => {
+            await Promise.all(
+                testData.map(async (example) => {
+                    return await service.create(test.entity, example);
+                })
+            );
+            await service.findAll(test.entity, { path: "test" }, paginateConfig).then((results) => {
+                expect(results).toBeDefined();
+                expect(results.data).toBeDefined();
+                expect(results.data.length).toEqual(testData.length);
+                for (let testEntity of testData) {
+                    const entity = results.data.find((result) => {
+                        let found: number = 1;
+                        for (let key in result) {
+                            if (key !== "id" && result[key] !== testEntity[key]) found = found * 0;
+                        }
+                        return found ? true : false;
+                    });
+                    expect(entity).toBeDefined();
+                }
+            });
         });
-    });
 
-    it("should delete", async () => {
-        const example = testData[0];
-        const created = await service.create(Example, example);
-        await service.delete(Example, created.id).then((result) => {
-            expect(result).toBeNull();
-            return result;
+        it("should update", async () => {
+            const example = JSON.parse(JSON.stringify(testData[0]));
+            const updatedExample = { ...example, firstName: "Updated" };
+            const created = await service.create(test.entity, example);
+            await service.update(test.entity, created.id, updatedExample).then((result) => {
+                expect(result).toBeNull();
+            });
         });
-    });
+
+        it("should delete", async () => {
+            const example = testData[0];
+            const created = await service.create(test.entity, example);
+            await service.delete(test.entity, created.id).then((result) => {
+                expect(result).toBeNull();
+            });
+        });
+    }
 
     afterEach(async () => {
         await dataSource.destroy();
